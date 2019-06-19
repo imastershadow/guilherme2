dbfea960d35acb6935e2ac0071285b000dcc25f7
imastershadow <51271070+imastershadow@users.noreply.github.com>
2019-06-19 00:24:27 -0300

    Update Sapato40.java

--- FactoryMethod1/src/factorymethod/Sapato40.java
+++ FactoryMethod1/src/factorymethod/Sapato40.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package factorymethod;
 
-/**
- *
- * @author Rafael
- */
 public class Sapato40 extends FabricaSapato{
     
     public Sapato40(String marca){
