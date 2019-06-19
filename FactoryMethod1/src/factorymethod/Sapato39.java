546bfe4ea7cdf8a5eb63cbfe633d67d444e0b5b8
imastershadow <51271070+imastershadow@users.noreply.github.com>
2019-06-19 00:24:09 -0300

    Update Sapato39.java

--- FactoryMethod1/src/factorymethod/Sapato39.java
+++ FactoryMethod1/src/factorymethod/Sapato39.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package factorymethod;
 
-/**
- *
- * @author 0422
- */
+
 public class Sapato39 extends FabricaSapato{
     
     
