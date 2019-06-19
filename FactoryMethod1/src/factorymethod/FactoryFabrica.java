27a9f54657eac54a03014c2335c5d423a7f3c2b6
imastershadow <51271070+imastershadow@users.noreply.github.com>
2019-06-19 00:23:33 -0300

    Update FactoryFabrica.java

--- FactoryMethod1/src/factorymethod/FactoryFabrica.java
+++ FactoryMethod1/src/factorymethod/FactoryFabrica.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package factorymethod;
 
-/**
- *
- * @author Rafael
- */
+
 public class FactoryFabrica {
     public FabricaSapato getFabricaSapato(String marca, String tamanho){
         if (marca.equals("Nike"))
